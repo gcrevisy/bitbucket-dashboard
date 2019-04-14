9ced1251387e1ba07762edda29ed032648de8b41
gcrevisy <gcrevisy@outlook.com>
2019-04-14 16:35:04 +0200

    ajout projet

---
+++ bitbucket-dashboard/src/main/java/fr/alteca/dashboard/App.java
@@ -0,0 +1,13 @@
+package fr.alteca.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "fr.alteca.dashboard.controller")
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
