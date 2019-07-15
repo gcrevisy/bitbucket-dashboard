799dd44a4b04da08143e5d470a29fde5b4d71d2b
gcrevisy <gcrevisy@outlook.com>
2019-07-15 22:25:02 +0200

    correction TU

--- bitbucket-dashboard-service/src/test/java/fr/alteca/dashboard/service/impl/BrancheServiceImplTest.java
+++ bitbucket-dashboard-service/src/test/java/fr/alteca/dashboard/service/impl/BrancheServiceImplTest.java
@@ -3,6 +3,8 @@ package fr.alteca.dashboard.service.impl;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import org.junit.Test;
@@ -43,11 +45,20 @@ public class BrancheServiceImplTest {
         return new BrancheDao() {
             @Override
             public List<Branche> listerBranches(Contexte contexte) {
+
+                GregorianCalendar date = (GregorianCalendar) GregorianCalendar.getInstance();
+
                 List<Branche> liste = new ArrayList<Branche>();
-                liste.add(new Branche("master", null, "gcrevisy"));
-                liste.add(new Branche("feature/toto", null, "gcrevisy"));
-                liste.add(new Branche("toto", null, "gcrevisy"));
-                liste.add(new Branche("", null, "gcrevisy"));
+                liste.add(new Branche("master", date, "gcrevisy"));
+
+                date.add(Calendar.DAY_OF_MONTH, -5);
+                liste.add(new Branche("feature/toto", date, "gcrevisy"));
+
+                date.add(Calendar.DAY_OF_MONTH, -10);
+                liste.add(new Branche("toto", date, "gcrevisy"));
+
+                date.add(Calendar.DAY_OF_MONTH, -15);
+                liste.add(new Branche("", date, "gcrevisy"));
                 return liste;
             }
         };
